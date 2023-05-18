e30ac53251bd035b935f095072d8e4dc1c295b2d
Paul Cheng <chengyuejia@foxmail.com>
2023-05-18 15:09:21 +0800

    Add more io test

--- src/tests/utils.rs
+++ src/tests/utils.rs
@@ -1,10 +1,12 @@
 #[cfg(test)]
 mod test {
     use crate::bn256::{Fq, Fr};
-    use crate::utils::{read_field_from_file, write_field_into_file};
+    use crate::utils::{
+        read_field_from_file, read_fields_from_file, write_field_into_file, write_fields_into_file,
+    };
+    use pasta_curves::arithmetic::FieldExt;
     use std::fs::File;
     use std::io::{stdout, Read, Write};
-    use pasta_curves::arithmetic::FieldExt;
 
     const fr_file_name: &str = "./fr_test_file.bin";
     const fq_file_name: &str = "./fq_test_file.bin";
@@ -47,5 +49,19 @@ mod test {
         println!("{:?}", fq.0);
         println!("{:?}", fq);
         println!("{:?}", fq.convert_to_bytes());
+    }
+
+    #[test]
+    fn test_io_fqs() {
+        let fq = Fq::one();
+        let fq2 = Fq::zero();
+
+        println!("{:?}", fq.0);
+        println!("{:?}", fq2.0);
+        write_fields_into_file(fq_file_name, &[fq, fq2]);
+
+        let fqs: Vec<Fq> = read_fields_from_file(fq_file_name, 2);
+        println!("{:?}", fqs.get(0).unwrap().0);
+        println!("{:?}", fqs.get(1).unwrap().0);
     }
 }
